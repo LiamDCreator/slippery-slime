9fa9ef79af564f0ddcc42755d26a40fe62d48734
LiamDCreator <Liamduijn@gmail.com>
2025-04-13 23:34:27 +0200

    added dashes to 4 directions + slimesprite

--- Assets/playerScript.cs
+++ Assets/playerScript.cs
@@ -1,34 +1,80 @@
 using UnityEngine;
 
 public class playerScript : MonoBehaviour
-{
-     private Rigidbody2D rb;
+{ 
+    private Rigidbody2D rb;
     private bool isGrounded;
-    private int jumpsRemaining = 2;
+    private bool wasGrounded;
+    public int jumpsRemaining = 2;
+
+    [SerializeField] private float forceAmount = 10f;
     [SerializeField] private float jumpForce;
     [SerializeField] private Transform groundCheck;
-    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private float groundCheckRadius = 0.3f;
     [SerializeField] private LayerMask groundMask;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-          rb = GetComponent<Rigidbody2D>();
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-   
+        playerJump();  // input checking here is fine
+        playerDash();
+    }
+
+    void FixedUpdate()
+    {
+        CheckGrounded(); // physics check in FixedUpdate
+    }
+
+    private void CheckGrounded()
+    {
+        wasGrounded = isGrounded;
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundMask);
-     if (isGrounded){
-        jumpsRemaining = 2;
-     }
-       
+
+        // Reset jumps only when landing
+        if (!wasGrounded && isGrounded)
+        {
+            jumpsRemaining = 2;
+        }
+    }
+
+    private void playerJump()
+    {
         if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)) && jumpsRemaining > 0)
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce); // jump
-            jumpsRemaining -= jumpsRemaining;
-           
-        }}
-}
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce); // fixed to velocity
+            jumpsRemaining--;
+        }
+    }
+
+    private void playerDash()
+    {
+        if (jumpsRemaining <= 1 && jumpsRemaining > 0)
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                rb.AddForce(Vector2.right * forceAmount, ForceMode2D.Impulse);
+                jumpsRemaining--;
+            }
+
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                rb.AddForce(Vector2.left * forceAmount, ForceMode2D.Impulse);
+                jumpsRemaining--;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                rb.AddForce((Vector2.left + Vector2.up).normalized * forceAmount, ForceMode2D.Impulse);
+                jumpsRemaining--;
+            }
+             if (Input.GetKeyDown(KeyCode.E))
+            {
+                rb.AddForce((Vector2.right + Vector2.up).normalized * forceAmount, ForceMode2D.Impulse);
+                jumpsRemaining--;
+            }
+        }
+    }}
