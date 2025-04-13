38a249cad87c61928c0ef2a8dbb5220f9c13a5ed
LiamDCreator <Liamduijn@gmail.com>
2025-04-13 23:55:44 +0200

    added adventurer enemy

---
+++ Assets/adventurerEnemyScript.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class adventurerEnemyScript : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    private bool leftOrRight;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if(transform.position.x > 0){
+            leftOrRight = true;
+        } else if (transform.position.x < 0) {
+            leftOrRight = false;}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       move();
+    }
+    private void move()
+    {
+        if(leftOrRight == true){
+  transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        } else if (leftOrRight == false){
+  transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+        }
+   
+             if(transform.position.x < -15 || transform.position.x > 15){
+            Destroy(gameObject);
+        }
+    }
+}
