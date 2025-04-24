df4cb85d5fa6842634835c02f656e7aa29a69d0a
LiamDCreator <Liamduijn@gmail.com>
2025-04-24 11:49:16 +0200

    added score4 UI

--- Assets/scripts/gameManager.cs
+++ Assets/scripts/gameManager.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class gameManager : MonoBehaviour
 {
     [SerializeField] private int score = 0;
+    [SerializeField] private Text scoreText;
 
     // Method to increment the score
     public void AddScore(int amount)
     {
         score += amount;
         Debug.Log("Score: " + score); // Log the updated score
+        if (scoreText != null)
+        {
+            scoreText.text = "" + score; // Update the text component
+        }
     }
 }
