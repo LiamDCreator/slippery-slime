bded506885e4bee98b0499ead70c67771f8047be
LiamDCreator <Liamduijn@gmail.com>
2025-06-09 17:40:15 +0200

    added clash gamemode + ordered folders

--- Assets/scripts/sceneLoader.cs
+++ Assets/scripts/sceneLoader.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class sceneLoader : MonoBehaviour
 {
     [SerializeField] private string BasicLevel;
+    [SerializeField] private string ClashLevel;
 
     // Call this method from the button's OnClick() event
     public void LoadScene()
@@ -11,6 +12,19 @@ public class sceneLoader : MonoBehaviour
         if (!string.IsNullOrEmpty(BasicLevel))
         {
             SceneManager.LoadScene(BasicLevel);
+            Time.timeScale = 1f;
+
+        }
+        else
+        {
+            Debug.LogWarning("Scene name is not set in SceneLoader!");
+        }
+    }
+    public void LoadClashScene()
+    {
+        if (!string.IsNullOrEmpty(ClashLevel))
+        {
+            SceneManager.LoadScene(ClashLevel);
             Time.timeScale = 1f;
 
         }
