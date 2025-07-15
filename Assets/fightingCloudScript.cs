91a1bb1914d73bdd5eb9923130e224a9bd746908
LiamDCreator <Liamduijn@gmail.com>
2025-07-15 14:15:30 +0200

    begin fightingsystem rework

--- Assets/fightingCloudScript.cs
+++ Assets/fightingCloudScript.cs
@@ -1,19 +1,65 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class fightingCloudScript : MonoBehaviour
 {
+    public List<EnemyBase> monsters = new List<EnemyBase>();
+    public List<EnemyBase> humans = new List<EnemyBase>();
+    [SerializeField] private int totalMonsterStrength = 0;
+    [SerializeField] private int totalHumanStrength = 0;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+
+
+    }
+    public void AddFighter(EnemyBase enemy)
+    {
+        if (enemy.faction == Faction.Monster && !monsters.Contains(enemy))
+        {
+            monsters.Add(enemy);
+            totalMonsterStrength += enemy.strength; // Update total immediately
+        }
+        else if (enemy.faction == Faction.Human && !humans.Contains(enemy))
+        {
+            humans.Add(enemy);
+            totalHumanStrength += enemy.strength; // Update total immediately
+        }
+
+        enemy.gameObject.SetActive(false);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), collision.collider, true);
+            EnemyBase enemy = collision.gameObject.GetComponent<EnemyBase>();
+            if (enemy != null)
+            {
+                AddFighter(enemy);
+            }
         }
     }
-    private void OnCollisionStay2D(Collision2D collision)
+    private IEnumerator fightingLogic()
     {
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), collision.collider, true);
-        }
+        // Reset totals before calculation
+        totalMonsterStrength = 0;
+        totalHumanStrength = 0;
+
+        // Sum strengths for monsters
+        foreach (var enemy in monsters)
+            totalMonsterStrength += enemy.strength;
+
+        // Sum strengths for humans
+        foreach (var enemy in humans)
+            totalHumanStrength += enemy.strength;
+
+        yield return null; // If you want to yield for coroutine compatibility
     }
 }
