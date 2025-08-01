d912269bab38eed133bb540af0b5fa8111c7c4a2
LiamDCreator <Liamduijn@gmail.com>
2025-08-01 14:54:03 +0200

    Added nuke powerup + put scripts in script folder

---
+++ Assets/scripts/nukePowerUpScript.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class nukePowerUpScript : MonoBehaviour
+{
+
+    public gameManager gameManager;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            KillAllEnemies();
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), collision.collider);
+
+        }
+    }
+
+    private void KillAllEnemies()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (GameObject enemy in enemies)
+        {
+            Destroy(enemy);
+            gameManager.score += 1;
+            gameManager.scoreText.text = "" + gameManager.score;
+
+        }
+
+    }
+}
