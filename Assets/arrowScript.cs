5601c50b369c2e1ac976f9d4ea835ecfea7a5142
LiamDCreator <Liamduijn@gmail.com>
2025-11-10 16:50:29 +0100

    Added arrow strength + fixed enemy mass

--- Assets/arrowScript.cs
+++ Assets/arrowScript.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class arrowScript : MonoBehaviour
 {
     private Rigidbody2D rb;
+    [SerializeField] private int arrowStrength;
 
     [SerializeField] private float moveSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -26,7 +27,21 @@ public class arrowScript : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // Check if the arrow hit an enemy
+        EnemyBase enemy = collision.gameObject.GetComponent<EnemyBase>();
+        if (enemy != null)
+        {
+            // Damage the enemy by reducing its strength
+            enemy.strength -= arrowStrength;
 
+            // If enemy strength drops to 0 or below, destroy the enemy
+            if (enemy.strength <= 0)
+            {
+                Destroy(collision.gameObject);
+            }
+        }
+
+        // Destroy the arrow regardless of what it hit
         Destroy(gameObject);
     }
 }
